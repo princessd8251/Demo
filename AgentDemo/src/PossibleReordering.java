dafd8349922871b1f9440281fabeb7ec10011306
zhaoxk <xkzalpha@gmail.com>
2014-04-29 09:53:29 +0800

    update

--- AgentDemo/src/PossibleReordering.java
+++ AgentDemo/src/PossibleReordering.java
@@ -8,34 +8,30 @@ public class PossibleReordering {
 		
 		Thread one = new Thread(new Runnable() {
 			public void run() {
-				for(int i=0; i<1; i++) {
-					a = 1;
-					x = b;
-				}
+                a = 1;
+                x = b;
 			}
 		});
 		
 		Thread other = new Thread(new Runnable() {
 			
 			public void run() {
-				for (int i =0; i<1; i++) {
-					b = 1;
-					y = a;
-					a = 1;
-					x = b;
-					a = 1;
-					x = b;
-					a = 1;
-					x = b;
-					a = 1;
-					x = b;
-					a = 1;
-					x = b;
-					a = 1;
-					x = b;
-					a = 1;
-					x = b;
-				}
+                b = 1;
+                y = a;
+                a = 1;
+                x = b;
+                a = 1;
+                x = b;
+                a = 1;
+                x = b;
+                a = 1;
+                x = b;
+                a = 1;
+                x = b;
+                a = 1;
+                x = b;
+                a = 1;
+                x = b;
 			}
 		});
 		
